8e9d180a8bb3c7d8c3bc4ec42b08db9f382b2048
Sergio Flórez <sergiozerolf@gmail.com>
2019-07-22 13:46:51 +0200

    Behavior refactored into a class. implemented multiple exports in one execution

--- graficador.py
+++ graficador.py
@@ -6,140 +6,128 @@ import plotly as py
 import yaml
 
 
-# class GeneradorGraficas:
-#     def __init__(self):
+class GeneradorGraficas:
+    urls_ficheros_propiedades = []
 
+    def __init__(self, urls_ficheros_propiedades):
+        self.urls_ficheros_propiedades.extend(urls_ficheros_propiedades)
 
-'''
-MODIFICAR SOLAMENTE ÉSTE VALOR SEGÚN EL FICHERO DE PROPIEDADES DE CADA EJECUCIÓN
-'''
-URL_FICHERO_PROPIEDADES = 'ejecuciones/analisis parametrico/' \
-                          'SVNS/' \
-                          'alphas.parte2/' \
-                          'config.yaml'
+    def execute(self):
+        for url_params in self.urls_ficheros_propiedades:
+            param = yaml.load(open(url_params, 'r'), Loader=yaml.Loader)
+            print('LEIDO FICHERO PROPIEDADES ' + url_params)
 
+            # crear el directorio de salida en caso de que no exista
+            output_path = param["input_base_url"] + param["output_path"]
+            if not os.path.exists(output_path):
+                os.mkdir(output_path)
 
+            '''direccion URL base donde obtener los ficheros'''
+            url = param["input_base_url"]
 
+            self.exportar(param, url, output_path)
 
-param = yaml.load(open(URL_FICHERO_PROPIEDADES, 'r'), Loader=yaml.Loader)
-print('LEIDO FICHERO PROPIEDADES '+ URL_FICHERO_PROPIEDADES)
+    def inicializar_df(self, param, url, num_str):
+        filename = param["input_name"] + num_str + '.csv'
+        print('DATOS LEIDOS DE ' + url + filename)
+        return pd.read_csv(url + filename, sep=';', names=['iteracion', 'tiempo (ms)', 'fitness mejor', 'tamaño',
+                                                           'numIterSinMejora', 'vecindad', 'fitness sol actual',
+                                                           'fitness BL', 'distancia'],
+                           skiprows=[0])
 
-# crear el directorio de salida en caso de que no exista
-output_path = param["input_base_url"] + param["output_path"]
-if not os.path.exists(output_path):
-    os.mkdir(output_path)
+    def leer_datos(self, param, url, id_list):
+        shift = param["start_id"]
+        num_iter_max = 0
+        plot_info = []
 
-# obtención de datos
-# def leer_datos(url, number_str):
+        for i in range(shift, len(id_list) + 1 + shift):
+            df = self.inicializar_df(param, url, str(i))
+            plot_info.append(df['fitness mejor'])
 
+            if len(df['iteracion']) > num_iter_max:
+                num_iter_max = len(df['iteracion'])
 
-'''direccion URL base donde obtener los ficheros'''
-URL = param["input_base_url"]
+        plot_info.append([i for i in range(0, num_iter_max)])  # componente 0 = eje X
+        return plot_info
 
-
-def inicializar_df(num_str):
-    filename = param["input_name"] + num_str + '.csv'
-    print('DATOS LEIDOS DE ' + URL + filename)
-    return pd.read_csv(URL + filename, sep=';', names=['iteracion', 'tiempo (ms)', 'fitness mejor', 'tamaño',
-                                                       'numIterSinMejora', 'vecindad', 'fitness sol actual',
-                                                       'fitness BL', 'distancia'],
-                       skiprows=[0])
-
-
-def leer_datos(id_list):
-    shift = param["start_id"]
-    num_iter_max = 0
-    plot_info = []
-
-    for i in range(shift, len(id_list) + 1 + shift):
-        df = inicializar_df(str(i))
-        plot_info.append(df['fitness mejor'])
-
-        if len(df['iteracion']) > num_iter_max:
-            num_iter_max = len(df['iteracion'])
-
-    plot_info.append([i for i in range(0, num_iter_max)])  # componente 0 = eje X
-    return plot_info
-
-
-# layout
-def create_layout(y_name):
-    # noinspection PyUnresolvedReferences
-    return go.Layout(
-        paper_bgcolor='white',
-        plot_bgcolor='white',
-        legend_orientation="h",
-        title=dict(text=param["fig_title"], x=0.5, xanchor="center", y=0.9),
-        xaxis=dict(showgrid=False, gridwidth=1, gridcolor='lightgray', title_text='Iteraciones'),
-        yaxis=dict(showgrid=True, gridwidth=1, gridcolor='lightgray', tick0=0, dtick=0.05, title_text=y_name)
-    )
-
-
-def add_lines(fig, valores, mejor_fitness, x):
-    p = param["parametro"]
-    for c, n in zip(mejor_fitness, valores):
+    # layout
+    def create_layout(self, param):
+        y_name = param["name_y_axis"]
         # noinspection PyUnresolvedReferences
-        fig.add_trace(go.Scatter(x=x, y=c,
-                                 # line=dict(
-                                 # color='firebrick',
-                                 # width=2
-                                 # ),
-                                 name=p + str(n)))
+        return go.Layout(
+            paper_bgcolor='white',
+            plot_bgcolor='white',
+            legend_orientation="h",
+            title=dict(text=param["fig_title"], x=0.5, xanchor="center", y=0.9),
+            xaxis=dict(showgrid=False, gridwidth=1, gridcolor='lightgray', title_text='Iteraciones'),
+            yaxis=dict(showgrid=True, gridwidth=1, gridcolor='lightgray', tick0=0, dtick=0.05, title_text=y_name)
+        )
 
+    def add_lines(self, param, fig, valores, mejor_fitness, x):
+        p = param["parametro"]
+        for c, n in zip(mejor_fitness, valores):
+            # noinspection PyUnresolvedReferences
+            fig.add_trace(go.Scatter(x=x, y=c,
+                                     # line=dict(
+                                     # color='firebrick',
+                                     # width=2
+                                     # ),
+                                     name=p + str(n)))
 
-# %%
+    def exportar(self, param, url, output_path):
+        valores = param["valores"]
+        plot_info = self.leer_datos(param, url, range(param["start_id"], param["start_id"] + len(valores) - 1))
 
+        print('EXPORTANDO (' + output_path + ') ... ', end='')
+        # Definir grafica
+        # noinspection PyUnresolvedReferences
+        fig = go.Figure(layout=self.create_layout(param))
 
-def exportar():
-    valores = param["valores"]
-    plot_info = leer_datos(range(param["start_id"], param["start_id"] + len(valores) - 1))
+        self.add_lines(param, fig, valores, plot_info[:-1], plot_info[-1])
 
-    print('\nEXPORTANDO ('+output_path+') ... ', end='')
-    # Definir grafica
-    # noinspection PyUnresolvedReferences
-    fig = go.Figure(layout=create_layout(param["name_y_axis"]))
-
-    add_lines(fig, valores, plot_info[:-1], plot_info[-1])
-
-    # Add range slider
-    # noinspection PyUnresolvedReferences
-    fig.update_layout(
-        xaxis=go.layout.XAxis(
-            rangeslider=dict(
-                visible=param["show_slider"]
-            )
-        )  # , xaxis_domain=[0, 1]
-    )
-
-    # graficar
-
-    fig_name = param["fig_name"]
-
-    # fig.show(renderer='browser', scale=1.25, width=800,
-    #          height=500)  # default size: width=700, height=450, scale=None
-    # fig.show(renderer='notebook', scale=1.25,  width=800, height=500) # default size: width=700, height=450, scale=None
-
-    # Dynamic format
-    py.io.write_html(fig, file=output_path + fig_name + '.html', auto_open=False)
-
-    # en el output estatico no queremos Slider en ningun caso
-    if param["show_slider"]:
+        # Add range slider
+        # noinspection PyUnresolvedReferences
         fig.update_layout(
             xaxis=go.layout.XAxis(
                 rangeslider=dict(
-                    visible=False
+                    visible=param["show_slider"]
                 )
             )  # , xaxis_domain=[0, 1]
         )
 
-    # LaTeX format
-    fig.write_image(output_path + fig_name + ".eps", scale=7)
+        # graficar
 
-    # Static format
-    fig.write_image(output_path + fig_name + ".png", scale=10)
+        fig_name = param["fig_name"]
 
-    print('PROCESAMIENTO FINALIZADO')
+        # fig.show(renderer='browser', scale=1.25, width=800,
+        #          height=500)  # default size: width=700, height=450, scale=None
+        # fig.show(renderer='notebook', scale=1.25,  width=800, height=500) # default size: width=700, height=450, scale=None
+
+        # Dynamic format
+        py.io.write_html(fig, file=output_path + fig_name + '.html', auto_open=False)
+
+        # en el output estatico no queremos Slider en ningun caso
+        if param["show_slider"]:
+            fig.update_layout(
+                xaxis=go.layout.XAxis(
+                    rangeslider=dict(
+                        visible=False
+                    )
+                )  # , xaxis_domain=[0, 1]
+            )
+
+        # LaTeX format
+        fig.write_image(output_path + fig_name + ".eps", scale=7)
+
+        # Static format
+        fig.write_image(output_path + fig_name + ".png", scale=10)
+
+        print('PROCESAMIENTO FINALIZADO\n')
 
 
 if __name__ == '__main__':
-    exportar()
+    url_fichero_propiedades = [
+        'ejecuciones/analisis parametrico/SVNS/alphas/config.yaml',
+        'ejecuciones/analisis parametrico/SVNS/alphas.parte2/config.yaml',
+    ]
+    GeneradorGraficas(url_fichero_propiedades).execute()
