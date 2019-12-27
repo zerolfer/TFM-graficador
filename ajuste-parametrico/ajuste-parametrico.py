41dc83ee9882befecb170cd4cc85764253f3daf7
Sergio Flórez <sergiozerolf@gmail.com>
2019-12-27 14:05:19 +0100

    Reduced bars plot x-axis min/max values

--- ajuste-parametrico/ajuste-parametrico.py
+++ ajuste-parametrico/ajuste-parametrico.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from os import listdir, path, makedirs
+from os import listdir, path, makedirs, walk
 import matplotlib.pyplot as plt
 from IPython.display import display
 
@@ -35,25 +35,52 @@ def calcular_datos(base_path, sub_paths):
     return datos_medios, datos_ordenados
 
 
-def dibujar_datos(datos, out_path, title="", multiple=True):
+def dibujar_datos(datos, out_path, file_name, figure_title="", multiple=True):
     columns = ["fitness total", "fitness 1", "fitness 2", "fitness 3", "fitness 4"]
     if not multiple:
         columns = ["fitness total"]
-    ax = datos[columns].plot.bar(title=title)
+
+    ax = datos[columns].plot.bar(title=figure_title)
+
+    if not multiple:
+        low = datos[columns[0]].min()
+        high = datos[columns[0]].max()
+    else:
+        low = 1
+        high = 0
+        for col in columns:
+            min = datos[col].min()
+            max = datos[col].max()
+
+            if min < low: low = min
+            if max > high: high = max
+
+    multiplicador=0.01
+    if not multiple: multiplicador=0.01
+
     for p in ax.patches:
         ax.annotate(str(round(p.get_height(), 3)),
-                    (p.get_x() * 1.005, p.get_height() * 1.005))
+                    (p.get_x() * 1.0005, p.get_height() + (high-low)*multiplicador))
+
     plt.legend(loc='lower left', bbox_to_anchor=(1.0, 0.0))
-    plt.savefig(out_path+str(title) + "_bars.png", dpi=150)
+    plt.ylim([low - 0.1 * (high - low), high + 0.1 * (high - low)])
+    plt.xticks(range(len(datos)), datos.index.tolist(), rotation=0)
+    plt.savefig(out_path + str(file_name) + "_bars.png", dpi=150)
     plt.show()
 
-    datos[columns].plot(style='.-', title=title)
+    datos[columns].plot(style='.-', title=figure_title)
     plt.legend(loc='lower left', bbox_to_anchor=(1.0, 0.0))
-    plt.savefig(out_path + str(title) + "_lines.png", dpi=150)
+    plt.xticks(range(len(datos)), datos.index.tolist(), rotation=0)
+    plt.savefig(out_path + str(file_name) + "_lines.png", dpi=150)
     plt.show()
 
 
 def ajuste_parametrico(base_path, sub_paths, out_path, parametro):
+
+    # if sub_paths is "":
+    #     sub_paths=get_subpaths(base_path, key=sub_paths_type)
+    print(sub_paths)
+
     # HACER MEDIAS Y MOSTRAR DATOS
     datos, datos_ordenados = calcular_datos(base_path, sub_paths)
 
@@ -64,5 +91,8 @@ def ajuste_parametrico(base_path, sub_paths, out_path, parametro):
     datos.to_csv(out_path + parametro + ".csv", sep=";", encoding="UTF-8")
 
     # REPRESENTAR GRAFICAMENTE
-    dibujar_datos(datos,           out_path, title="Datos no ordenados", multiple=False)
-    dibujar_datos(datos_ordenados, out_path, title="Datos ordenados por mejor fitness")
+    dibujar_datos(datos, out_path, file_name="Datos no ordenados", figure_title=parametro, multiple=False)
+    dibujar_datos(datos_ordenados, out_path, figure_title=parametro, file_name="Datos ordenados por mejor fitness")
+
+def get_subpaths(path, key=str.lower):
+    return sorted(next(walk(path))[1], key=key)
