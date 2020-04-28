49bb34dfcf87d968cf07194e0688ac9776ef3dca
Sergio Flórez <sergiozerolf@gmail.com>
2020-04-28 14:15:43 +0200

    Implemented the skip of the log file and sort c riteria. Added parameter tunning files

--- ajuste-parametrico/ajuste-parametrico.py
+++ ajuste-parametrico/ajuste-parametrico.py
@@ -7,8 +7,9 @@ from IPython.display import display
 def cargar_ejecuciones(path):
     dfs = []
     for ejecucion in listdir(path):
-        df = pd.read_csv(path + "\\" + ejecucion, sep=';', encoding="utf-8")
-        dfs.append(df)
+        if "log" not in ejecucion:
+            df = pd.read_csv(path + "\\" + ejecucion, sep=';', encoding="utf-8")
+            dfs.append(df)
     return dfs
 
 
@@ -16,21 +17,21 @@ def procesar_medias(dfs):
     lista = []
     for df in dfs:
         df.loc[df['fitness 1'] <= 0.0, 'fitness 1'] = 1
-        lista.append(df.iloc[-1])
+        lista.append(df.iloc[-1]) # Tomamos el último valor del df actual
     df_concat = pd.DataFrame(data=lista, index=range(0, len(lista)))
     return df_concat.mean()
 
 
-def calcular_datos(base_path, sub_paths):
+def calcular_datos(base_path, sub_paths, sort_criteria='fitness total'):
     datos_medios_dict = {}
 
     for sub_path in sub_paths:
         dfs = cargar_ejecuciones(base_path + "\\" + sub_path)
         datos_medios_dict[sub_path] = procesar_medias(dfs)
 
-    # presentar_datos_medios, ordenador de mejor a peor
+    # presentar_datos_medios, ordenados de mejor a peor
     datos_medios = pd.DataFrame(data=datos_medios_dict).transpose()
-    datos_ordenados = datos_medios.sort_values(by=['fitness total'])
+    datos_ordenados = datos_medios.sort_values(by=[sort_criteria])
     display(datos_ordenados)
     return datos_medios, datos_ordenados
 
@@ -74,14 +75,14 @@ def dibujar_datos(datos, out_path, file_name, figure_title="", multiple=True):
     plt.savefig(out_path + str(file_name) + "_lines.png", dpi=150)
     plt.show()
 
-def ajuste_parametrico(base_path, sub_paths, out_path, parametro, *args):
+def ajuste_parametrico(base_path, sub_paths, out_path, parametro, sort_criteria='fitness total', *args):
 
     # if sub_paths is "":
     #     sub_paths=get_subpaths(base_path, key=sub_paths_type)
     print(sub_paths)
 
     # HACER MEDIAS Y MOSTRAR DATOS
-    datos, datos_ordenados = calcular_datos(base_path, sub_paths)
+    datos, datos_ordenados = calcular_datos(base_path, sub_paths, sort_criteria)
 
     if not path.exists(out_path):
         makedirs(out_path)
